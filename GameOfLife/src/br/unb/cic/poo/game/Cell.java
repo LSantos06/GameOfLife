3982f27e6ae3ba73985c60cc128003fae910942d
Lucas Santos <lucasnascimentoss@gmail.com>
2016-05-01 17:06:30 -0300

    Statistics e Cell implementadas

--- GameOfLife/src/br/unb/cic/poo/game/Cell.java
+++ GameOfLife/src/br/unb/cic/poo/game/Cell.java
@@ -1,5 +1,25 @@
 package br.unb.cic.poo.game;
 
 public class Cell {
+    private boolean alive;
+    
+    public Cell() {
+    	alive = false;
+    }
 
+    public Cell(boolean status) {
+    	alive = status; 
+    }
+    
+    public boolean isAlive() {
+    	return alive;
+    }
+
+    public void kill() {
+    	this.alive = false;
+    }
+	
+    public void revive() {
+    	this.alive = true;
+    }
 }
