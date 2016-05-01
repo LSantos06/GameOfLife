853716dcdcf18cfb9ff803815116f1333e87dae1
Lucas Santos <lucasnascimentoss@gmail.com>
2016-05-01 16:06:19 -0300

    Spring Tutorial 09 - Initializing Collections

--- GameOfLife/src/br/unb/cic/poo/engines/GameEngine.java
+++ GameOfLife/src/br/unb/cic/poo/engines/GameEngine.java
@@ -2,13 +2,17 @@ package br.unb.cic.poo.engines;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class GameEngine {
 
 	public static void main(String[] args) {
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		Conway conway = (Conway)factory.getBean("conway");
+		//Conway conway = new Conway();
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");		
+		Conway conway = (Conway)context.getBean("conway");
 		conway.game();
 	}
 
---
+++ GameOfLife/src/br/unb/cic/poo/engines/HighLife.java
@@ -0,0 +1,21 @@
+package br.unb.cic.poo.engines;
+
+public class HighLife {
+	
+	private int alive;
+	private int dead;
+	
+	
+	public int getAlive() {
+		return alive;
+	}
+	public void setAlive(int alive) {
+		this.alive = alive;
+	}
+	public int getDead() {
+		return dead;
+	}
+	public void setDead(int dead) {
+		this.dead = dead;
+	}
+}
